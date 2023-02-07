1abac624af558b8a8ef12617e7f418fb52efbb35
Allie Davis <84688626+AllieDavis99@users.noreply.github.com>
2023-02-06 18:52:36 -0800

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,6 +1,19 @@
-CREATE OR REPLACE Customers();
+CREATE OR REPLACE Customers(
+  customer_id int(11) NOT NULL AUTO_INCREMENT UNIQUE,
+  customer_name varchar(255) NOT NULL,
+  phone_number int(11) NOT NULL,  
+  address varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
 
-CREATE OR REPLACE Floors();
+CREATE OR REPLACE Floors(
+  floor_id int(11) NOT NULL AUTO_INCREMENT UNIQUE,
+  occupied_rooms int(11),
+  empty_rooms int(11),
+  has_facilities bool NOT NULL,
+  PRIMARY KEY(floor_id)
+);
 
 CREATE OR REPLACE RoomTypes();
 
