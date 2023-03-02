43055eb93b1e8d9939f9056250bc33ab6f5dadc0
Allie <davisalt@oregonstate.edu>
2023-03-02 11:41:04 -0800

    modified:   index.js 	modified:   package.json || Added nodemon for convenience

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
@@ -13,20 +14,29 @@ var db = require('./database/db_connector')
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
 const { query } = require('express')
+
 app.engine('.hbs', engine({ extname: ".hbs" }));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');
 
+//
+//ROUTES
+//
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get('/customers', function (req, res) {
+//
+//CUSTOMERS
+//
+console.log('outside second get method')
+app.get('/customers.hbs', function (req, res) {
     let query1 = "SELECT * FROM customers;";
     db.pool.query(query1, function (error, rows, fields) {
-        res.render('customers', {data:rows})
+        res.render('customers.hbs', {data:rows})
     })
 });
 
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+app.listen(port, function(){
+    console.log('Server started at http://localhost:' + port);
+});
