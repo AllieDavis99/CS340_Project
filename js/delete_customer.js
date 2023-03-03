90903487663aa012496e917121be2e0e7ab38d33
Allie <davisalt@oregonstate.edu>
2023-03-02 16:04:02 -0800

    modified:   index.js 	new file:   js/delete_customer.js 	modified:   views/customers.hbs

---
+++ js/delete_customer.js
@@ -0,0 +1,26 @@
+function deleteCustomer(customerID){
+    let link = '/delete_customer/';
+    let data = {
+        id: customerID
+    };
+
+    $.ajax({
+        url: link,
+        type: 'DELETE',
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        success: function(result){
+            deleteRow(customerID);
+        }
+    });
+}
+
+function deleteRow(customerID){
+    let table = document.getElementById("customer-table");
+    for (let i = 0, row; row = table.rows[i]; i++){
+        if (table.rows[i].getAttribute("data-value") == personID){
+            table.deleteRow(i);
+            break;
+        }
+    }
+}
