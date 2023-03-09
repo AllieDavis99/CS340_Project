53d269ebd4eb06da7a79f029319c5546d59bedda
Karin Ocheretny <ocheretk@oregonstate.edu>
2023-03-09 12:48:45 -0800

    add room types

--- js/add_roomType.js
+++ js/add_roomType.js
@@ -4,25 +4,31 @@ add_customer_form.addEventListener("addRoomType", function (e) {
     e.preventDefault();
 
     //get data from form
-    let inputOcc = document.getElementById("occupied_rooms");
-    let inputEmpty = document.getElementById("empty_rooms");
-    let inputFacili = document.getElementById("has_facilities");
+    let inputName = document.getElementById("type_name");
+    let inputBed = document.getElementById("num_beds");
+    let inputBath = document.getElementById("num_baths");
+    let inputHaunted = document.getElementById("is_haunted");
+    let inputPrice = document.getElementById("price_per_night");
 
 
-    let occValue = inputOcc.value;
-    let emptyValue = inputEmpty.value;
-    let faciliValue = inputFacili.value;
+    let nameValue = inputName.value;
+    let bedValue = inputBed.value;
+    let bathValue = inputBath.value;
+    let hauntedValue = inputHaunted.value;
+    let priceValue = inputPrice.value;
 
 
     let data = {
-        occupied_rooms: occValue,
-        empty_rooms: emptyValue,
-        has_facilities: faciliValue,
+        type_name: nameValue,
+        num_beds: bedValue,
+        num_baths: bathValue,
+        is_haunted: hauntedValue,
+        price_per_night: priceValue,
     }
 
     //AJAX request
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/floors.hbs", true);
+    xhttp.open("POST", "/roomTypes.hbs", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
     xhttp.onreadystatechange = () => {
@@ -30,9 +36,11 @@ add_customer_form.addEventListener("addRoomType", function (e) {
             addRowToTable(xhttp.response);
 
             //clear input fields for next transaction
-            inputOcc.value = '';
-            inputEmpty.value = '';
-            inputFacili.value = '';
+            inputName.value = '';
+            inputBed.value = '';
+            inputBath.value = '';
+            inputHaunted.value = '';
+            inputPrice.value = '';
         }
 
         else if (xhttp.readyState == 4 && xhttp.status != 200) {
@@ -53,23 +61,29 @@ addRowToTable = (data) => {
     //create row and it's cells
     let row = document.createElement("TR");
     let idCell = document.createElement("TD");
-    let occCel = document.createElement("TD");
-    let emptyCell = document.createElement("TD");
-    let faciliCell = document.createElement("TD");
+    let nameCel = document.createElement("TD");
+    let bedCell = document.createElement("TD");
+    let bathCell = document.createElement("TD");
+    let hauntedCell = document.createElement("TD");
+    let priceCell = document.createElement("TD");
 
 
     //populate cells
     idCell.innerText = newRow.id;
-    occCel.innerText = newRow.occupied_rooms;
-    emptyCell.innerText = newRow.empty_rooms;
-    faciliCell.innerText = newRow.has_facilities;
+    nameCel.innerText = newRow.type_name;
+    bedCell.innerText = newRow.num_beds;
+    bathCell.innerText = newRow.num_baths;
+    hauntedCell.innerText = newRow.is_haunted;
+    priceCell.innerText = newRow.price_per_night;
 
 
     //add cells to row
     row.appendChild(idCell);
-    row.appendChild(occCel);
-    row.appendChild(emptyCell);
-    row.appendChild(faciliCell);
+    row.appendChild(nameCel);
+    row.appendChild(bedCell);
+    row.appendChild(bathCell);
+    row.appendChild(hauntedCell);
+    row.appendChild(priceCell);
 
     currentTable.appendChild(row);
 }
